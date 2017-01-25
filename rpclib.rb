0d1d836c50b3a013aca3cbbf12b535eff82d74ba
Timothee Cour <timothee.cour2@gmail.com>
2017-01-25 00:11:06 -0800

    rpclib fix

--- rpclib.rb
+++ rpclib.rb
@@ -1,9 +1,16 @@
 class Rpclib < Formula
   desc "TODO"
   homepage "https://github.com/rpclib/rpclib"
-  url "https://github.com/rpclib/rpclib",
-    :tag => "v1.0.0"
-  version "1.0.0"
+
+  stable do
+    url "https://github.com/rpclib/rpclib.git"
+    #url "https://github.com/rpclib/rpclib.git",
+    #    :tag => "v1.0.0"
+      #CHECKME
+      #:revision => "6bd588b3a570e3a9cd9cdd28a21b299387fe982d174ea1d8f47756d62c582b9f"
+    revision 2
+    version "1.0.0"
+  end
 
   depends_on "cmake" => :build
 
