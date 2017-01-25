b83836e6c10a52c8569f2eaa2571a07cdecf7ac2
Timothee Cour <timothee.cour2@gmail.com>
2017-01-25 00:16:08 -0800

    rpclib fix 2

--- rpclib.rb
+++ rpclib.rb
@@ -1,28 +1,19 @@
 class Rpclib < Formula
-  desc "TODO"
-  homepage "https://github.com/rpclib/rpclib"
+  desc "rpclib is a modern C++ msgpack-RPC server and client library"
+  homepage "http://rpclib.net"
+  url "https://github.com/rpclib/rpclib.git",
+    #TODO: why not: "v1.0.0" instead of "1.0.0" ? (cf camlistore.rb has :tag => "0.9" but boot2docker.rb has :tag => "v0.2.0")
+    :tag => "1.0.0"
+    :revision => "c8144d458c2b903c5a13cc08529d5866a012d095"
 
-  stable do
-    url "https://github.com/rpclib/rpclib.git"
-    #url "https://github.com/rpclib/rpclib.git",
-    #    :tag => "v1.0.0"
-      #CHECKME
-      #:revision => "6bd588b3a570e3a9cd9cdd28a21b299387fe982d174ea1d8f47756d62c582b9f"
-    revision 2
-    version "1.0.0"
-  end
+  version "1.0.0"
 
   depends_on "cmake" => :build
 
   def install
-    #args = ["-DINSTALL_EXTRA_LIBS=ON", "-DBUILD_UNIT_TESTS=OFF"]
-    args = []
+    args = std_cmake_args
     system "cmake", *args
     system "make"
     system "make", "install"
-
-    # system "./configure", "--prefix=#{prefix}"
-    # system "make", "install"
-
   end
 end
