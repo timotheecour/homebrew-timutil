bb1f42c47c63730ea027b33d4fc29555b04270f0
Timothee Cour <timothee.cour2@gmail.com>
2017-02-08 01:14:35 -0800

    copied rust.rb from linuxbrew

---
+++ rust.rb
@@ -0,0 +1,121 @@
+class Rust < Formula
+  desc "Safe, concurrent, practical language"
+  homepage "https://www.rust-lang.org/"
+
+  stable do
+    url "https://static.rust-lang.org/dist/rustc-1.15.0-src.tar.gz"
+    sha256 "33f3747d231ede34b56c6cc0ae6be8cbaa29d2fdb39d86f25693dceb9fc5f164"
+
+    resource "cargo" do
+      url "https://github.com/rust-lang/cargo.git",
+          :tag => "0.16.0",
+          :revision => "6e0c18cccc8b0c06fba8a8d76486f81a792fb420"
+    end
+
+    resource "racer" do
+      url "https://github.com/phildawes/racer/archive/2.0.5.tar.gz"
+      sha256 "370e8e2661b379185667001884b51bdc4b414abdc27bb9671513c1912ad8be25"
+    end
+  end
+
+  bottle do
+    sha256 "17210109dfa87d23dbca90befbae311d9e570d27f1122b710954b59e2ccea8fe" => :sierra
+    sha256 "881094ef4a3e3c0dda93a4976d8598415640899cee6845627075920c54229f3f" => :el_capitan
+    sha256 "a19d9b58a78caba0c738bebd06035a5cfb2b0129bac9a374a0199e010a195434" => :yosemite
+  end
+
+  head do
+    url "https://github.com/rust-lang/rust.git"
+
+    resource "cargo" do
+      url "https://github.com/rust-lang/cargo.git"
+    end
+  end
+
+  option "with-llvm", "Build with brewed LLVM. By default, Rust's LLVM will be used."
+  option "with-racer", "Build Racer code completion tool, and retain Rust sources."
+
+  depends_on "cmake" => :build
+  depends_on "pkg-config" => :run
+  depends_on "llvm" => :optional
+  # Fix https://github.com/Linuxbrew/homebrew-core/issues/1733: couldn't find required command: ar
+  depends_on "binutils" => :build unless OS.mac?
+  depends_on "openssl"
+  depends_on "libssh2"
+
+  conflicts_with "multirust", :because => "both install rustc, rustdoc, cargo, rust-lldb, rust-gdb"
+
+  # According to the official readme, GCC 4.7+ is required
+  fails_with :gcc_4_0
+  fails_with :gcc
+  ("4.3".."4.6").each do |n|
+    fails_with :gcc => n
+  end
+
+  resource "cargobootstrap" do
+    url "https://s3.amazonaws.com/rust-lang-ci/cargo-builds/fbeea902d2c9a5be6d99cc35681565d8f7832592/cargo-nightly-x86_64-apple-darwin.tar.gz"
+    version "2016-12-15"
+    sha256 "ad6c31b41fef1d68e4523eb7d090fe8103848f30eb5ac8cba5128b7c11ed23fc"
+  end
+
+  def install
+    # Reduce memory usage below 4 GB for Circle CI.
+    ENV["MAKEFLAGS"] = "-j12" if ENV["CIRCLECI"]
+    args = ["--prefix=#{prefix}"]
+    args << "--disable-rpath" if build.head?
+    args << "--enable-clang" if ENV.compiler == :clang
+    args << "--llvm-root=#{Formula["llvm"].opt_prefix}" if build.with? "llvm"
+    if build.head?
+      args << "--release-channel=nightly"
+    else
+      args << "--release-channel=stable"
+    end
+    system "./configure", *args
+    system "make"
+    system "make", "install"
+
+    resource("cargobootstrap").stage do
+      system "./install.sh", "--prefix=#{buildpath}/cargobootstrap"
+    end
+    ENV.prepend_path "PATH", buildpath/"cargobootstrap/bin"
+
+    resource("cargo").stage do
+      system "./configure", "--prefix=#{prefix}", "--local-rust-root=#{prefix}",
+                            "--enable-optimize"
+      system "make"
+      system "make", "install"
+    end
+
+    if build.with? "racer"
+      resource("racer").stage do
+        ENV.prepend_path "PATH", bin
+        cargo_home = buildpath/"cargo_home"
+        cargo_home.mkpath
+        ENV["CARGO_HOME"] = cargo_home
+        system bin/"cargo", "build", "--release", "--verbose"
+        (libexec/"bin").install "target/release/racer"
+        (bin/"racer").write_env_script(libexec/"bin/racer", :RUST_SRC_PATH => pkgshare/"rust_src")
+      end
+      # Remove any binary files; as Homebrew will run ranlib on them and barf.
+      rm_rf Dir["src/{llvm,test,librustdoc,etc/snapshot.pyc}"]
+      (pkgshare/"rust_src").install Dir["src/*"]
+    end
+
+    rm_rf prefix/"lib/rustlib/uninstall.sh"
+    rm_rf prefix/"lib/rustlib/install.log"
+  end
+
+  test do
+    system "#{bin}/rustdoc", "-h"
+    (testpath/"hello.rs").write <<-EOS.undent
+    fn main() {
+      println!("Hello World!");
+    }
+    EOS
+    system "#{bin}/rustc", "hello.rs"
+    assert_equal "Hello World!\n", `./hello`
+    system "#{bin}/cargo", "new", "hello_world", "--bin"
+    assert_equal "Hello, world!",
+                 (testpath/"hello_world").cd { `#{bin}/cargo run`.split("\n").last }
+  end
+end
