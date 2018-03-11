21713857bc188962d804ad6258c83c769e88c052
Timothee Cour <timothee.cour2@gmail.com>
2018-03-10 21:15:29 -0800

    fix #25098 ldc

---
+++ ldc.rb
@@ -0,0 +1,82 @@
+class Ldc < Formula
+  desc "Portable D programming language compiler"
+  homepage "https://wiki.dlang.org/LDC"
+  revision 1
+
+  stable do
+    url "https://github.com/ldc-developers/ldc/releases/download/v1.8.0/ldc-1.8.0-src.tar.gz"
+    sha256 "e421a1f4bbf97d173bd277125794862ca5b6a09409586b806cec23b922955c7f"
+
+    resource "ldc-lts" do
+      url "https://github.com/ldc-developers/ldc/releases/download/v0.17.5/ldc-0.17.5-src.tar.gz"
+      sha256 "7aa540a135f9fa1ee9722cad73100a8f3600a07f9a11d199d8be68887cc90008"
+    end
+  end
+
+  bottle do
+    sha256 "17535244b866762c63f0258f48cf237b57d2a5deb1d9c96bfaca5b7795c09d4c" => :high_sierra
+    sha256 "9737d2c51b6cb655f019792b808b64bd90858a4c0057bfc90b8844a2ec25d6e2" => :sierra
+    sha256 "afaf39315e93f3f67dafb1ede8d13fde8d3b92a02e26aca4ca825c2cd2a8958f" => :el_capitan
+  end
+
+  head do
+    url "https://github.com/ldc-developers/ldc.git", :shallow => false
+
+    resource "ldc-lts" do
+      url "https://github.com/ldc-developers/ldc.git", :shallow => false, :branch => "ltsmaster"
+    end
+  end
+
+  needs :cxx11
+
+  depends_on "cmake" => :build
+  depends_on "libconfig" => :build
+  depends_on "llvm@5"
+
+  def install
+    ENV.cxx11
+    (buildpath/"ldc-lts").install resource("ldc-lts")
+
+    cd "ldc-lts" do
+      mkdir "build" do
+        args = std_cmake_args + %W[
+          -DLLVM_ROOT_DIR=#{Formula["llvm@5"].opt_prefix}
+        ]
+        system "cmake", "..", *args
+        system "make"
+      end
+    end
+    mkdir "build" do
+      args = std_cmake_args + %W[
+        -DLLVM_ROOT_DIR=#{Formula["llvm@5"].opt_prefix}
+        -DINCLUDE_INSTALL_DIR=#{include}/dlang/ldc
+        -DD_COMPILER=#{buildpath}/ldc-lts/build/bin/ldmd2
+        -DLDC_WITH_LLD=OFF
+        -DRT_ARCHIVE_WITH_LDC=OFF
+      ]
+      # LDC_WITH_LLD see https://github.com/ldc-developers/ldc/releases/tag/v1.4.0 Known issues
+      # RT_ARCHIVE_WITH_LDC see https://github.com/ldc-developers/ldc/issues/2350
+
+      system "cmake", "..", *args
+      system "make"
+      system "make", "install"
+    end
+  end
+
+  test do
+    (testpath/"test.d").write <<~EOS
+      import std.stdio;
+      void main() {
+        writeln("Hello, world!");
+      }
+    EOS
+    system bin/"ldc2", "test.d"
+    assert_match "Hello, world!", shell_output("./test")
+    system bin/"ldc2", "-flto=thin", "test.d"
+    assert_match "Hello, world!", shell_output("./test")
+    system bin/"ldc2", "-flto=full", "test.d"
+    assert_match "Hello, world!", shell_output("./test")
+    system bin/"ldmd2", "test.d"
+    assert_match "Hello, world!", shell_output("./test")
+  end
+end
